4368b8fab0258d1e7380dae6483debcea6294d53
ihor <malishkoio@gmail.com>
2020-09-11 02:47:26 +0300

    Updates

---
+++ static/js/main.0481c1b9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-hw-03-image-finder"]=this["webpackJsonpreact-hw-03-image-finder"]||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),c=t(2);var i=function(){return a.a.createElement(a.a.Fragment,null,a.a.createElement("h2",null,"react-hw-03-img"))};t.n(c).a.render(a.a.createElement(i,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.0481c1b9.chunk.js.map
